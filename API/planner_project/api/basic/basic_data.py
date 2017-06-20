f80d687b311fb5599ea9948cd6d66ddb9408ef97
xuxujiang0 <6895285a>
2017-06-20 23:11:14 +0800

    基础数据文件

---
+++ API/planner_project/api/basic/basic_data.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+from flask import request
+from planner_project import app
+from planner_project.common import api_response,request_helper
+from planner_project.data_access import mysql
+from planner_project.sql.service import service_sql
+
+#服务区域列表
+@app.route("/basic/arealist", methods=['POST'])
+def area_list():
+    ApiResponse = api_response.ApiResponse()
+    data = mysql.get_list(service_sql.select_area_list,())
+    ApiResponse.message = "成功"
+    ApiResponse.status = 200
+    ApiResponse.data = data
+    return api_response.response_return(ApiResponse)
+
+#认证服务列表
+@app.route("/basic/servicelist", methods=['POST'])
+def service_list():
+    ApiResponse = api_response.ApiResponse()
+    data = mysql.get_list(service_sql.select_service_list,())
+    ApiResponse.message = "成功"
+    ApiResponse.status = 200
+    ApiResponse.data = data
+    return api_response.response_return(ApiResponse)
