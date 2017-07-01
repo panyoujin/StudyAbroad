689c15c202519258d5fb59765e34f788ea965d56
xuxujiang0 <6895285a>
2017-07-01 17:21:41 +0800

    新增注册验证码判断逻辑

--- API/planner_project/api/user/register.py
+++ API/planner_project/api/user/register.py
@@ -5,6 +5,7 @@ from planner_project import app
 from planner_project.common import api_response,custom_error
 from planner_project.data_access import mysql
 from planner_project.sql.user import user_sql
+from planner_project.sql.basic import verification_code_sql
 
 @app.route("/helloworld", methods=['GET'])
 def helloworld():
@@ -16,13 +17,25 @@ def register():
     ApiResponse = api_response.ApiResponse()
     Account = request.form.get("Account", type=str, default=None)
     Password = request.form.get("Password", type=str, default=None)
+    VCode = request.form.get("VCode", type=str, default=None)
     if Account == None:
         raise custom_error.CustomFlaskErr(status_code=500, message="账号不能为空")
     if Password == None:
         raise custom_error.CustomFlaskErr(status_code=500, message="密码不能为空")
+    if VCode == None:
+        raise custom_error.CustomFlaskErr(status_code=500, message="验证码不能为空")
+
     data_exists = mysql.get_list(user_sql.select_exists_user,(Account))
     if len(data_exists):
         raise custom_error.CustomFlaskErr(status_code=500, message="账号已经存在")
+
+    vocde_exists = mysql.get_object(verification_code_sql.select_verification_lastcode, (Account,1))
+    if vocde_exists== None :
+        raise custom_error.CustomFlaskErr(status_code=500, message="验证码已失效")
+    if vocde_exists["VCode"]!=VCode:
+        raise custom_error.CustomFlaskErr(status_code=500, message="验证码不正确")
+    #设置验证码已读
+    mysql.operate_object(verification_code_sql.update_verificatioin_vcode,(vocde_exists["Id"]))
 
     guid = str(uuid.uuid1())
     sql_list = [user_sql.insert_user,user_sql.insert_userinfo]
--- API/planner_project/sql/basic/verification_code_sql.py
+++ API/planner_project/sql/basic/verification_code_sql.py
@@ -2,4 +2,7 @@
 insert_verification_codel = "INSERT INTO Base_VerificationCode (`Phone`,`VCode`,`CodeType`,`CreateTime`) values ('%s','%s',%s,now())"
 
 #根据该手机 对应类型的最后一条短信验证码
-select_verification_lastcode="select Id,`VCode` from `Base_VerificationCode` where `Phone`='%s' and `IsRead`=1 and `IsDelete`=false and `CodeType`=%s order by `CreateTime` desc limit 1"
+select_verification_lastcode="select Id,`VCode` from `Base_VerificationCode` where `Phone`='%s' and `IsRead`=1 and `IsDelete`=false and `CodeType`=%s and ADDDATE(CreateTime,INTERVAL 5 MINUTE) >= NOW() order by `CreateTime` desc limit 1"
+
+#修改验证码已读
+update_verificatioin_vcode="UPDATE `Base_VerificationCode` SET IsRead=2,`ModifTime`=NOW() WHERE `Id`=%s"
