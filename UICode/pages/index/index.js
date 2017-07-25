0ba6fb79e8dc43f57aa56bc63e2997acf9cd7087
xuxujiang0 <1842125096@qq.com>
2017-07-25 18:57:36 +0800

    修改菜单

--- UICode/pages/index/index.js
+++ UICode/pages/index/index.js
@@ -3,43 +3,62 @@
 //JSON.stringify() JSON.parse()
 var app = getApp()
 Page({
-  data: {
-    imgUrls: [
-      { url: '', src: '/img/home/banner.png' }
-    ],
-    userInfo: {}
-  },
-  //事件处理函数
-  //banner图跳转
-  swipclick: function(e){
-    wx.switchTab({
-      url: e.currentTarget.dataset.url
-    })
-  },
-  //查询
-  searchBtnClick:function(){
-    wx.navigateTo({
-      url: "/pages/planner/plannerSearch/plannerSearch"
-    })
-  },
+    data: {
+        imgUrls: [
+            {url: '', src: '/img/home/banner.png'}
+        ],
+        userInfo: {},
+        hidden: true
+    },
+    //点击浮动按钮事件
+    alertContent: function (e) {
+        var that = this
+        that.setData({
+            hidden: false
+        })
+    },
+    //点击弹窗的确定事件
+    confirm: function (e) {
+        this.setData({
+            hidden: true
+        });
+    },
+    payPhoneNum: function (e) {
+        wx.makePhoneCall({
+            phoneNumber: '0752-123456',
+        })
+    },
+    //事件处理函数
+    //banner图跳转
+    swipclick: function (e) {
+        wx.switchTab({
+            url: e.currentTarget.dataset.url
+        })
+    },
+    //查询
+    searchBtnClick: function () {
+        wx.navigateTo({
+            url: "/pages/planner/plannerSearch/plannerSearch"
+        })
+    },
 
-  bindViewTap: function() {
-    wx.switchTab({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-      that.setData({
-        userInfo: app.globalData.userInfo
-      })
-    //调用应用实例的方法获取全局数据
-    // app.getUserInfo(function(userInfo){
-    //   //更新数据
-    //   that.setData({
-    //     userInfo:userInfo
-    //   })
-    // })
-  }
+    bindViewTap: function () {
+        wx.switchTab({
+            url: '../logs/logs'
+        })
+    },
+    onLoad: function () {
+        console.log('onLoad')
+        var that = this
+        that.setData({
+            userInfo: app.globalData.userInfo
+        })
+        //调用应用实例的方法获取全局数据
+        // app.getUserInfo(function(userInfo){
+        //   //更新数据
+        //   that.setData({
+        //     userInfo:userInfo
+        //   })
+        // })
+    }
 })
