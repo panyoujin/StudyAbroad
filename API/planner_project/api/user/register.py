236774e4135b08cdb3d6c1756643c860dfac42fd
xuxujiang0 <6895285a>
2017-08-27 15:25:04 +0800

    修改注册返回

--- API/planner_project/api/user/register.py
+++ API/planner_project/api/user/register.py
@@ -1,17 +1,20 @@
-#coding:utf-8
-from flask import request
+# coding:utf-8
+from flask import request, session
+import time
 import uuid
 from planner_project import app
-from planner_project.common import api_response,custom_error
+from planner_project.common import api_response, custom_error
 from planner_project.data_access import mysql
 from planner_project.sql.user import user_sql
 from planner_project.sql.basic import verification_code_sql
+
 
 @app.route("/helloworld", methods=['GET'])
 def helloworld():
     return "旭江超帅"
 
-#注册用户
+
+# 注册用户
 @app.route("/user/register", methods=['POST'])
 def register():
     ApiResponse = api_response.ApiResponse()
@@ -25,30 +28,35 @@ def register():
     if VCode == None:
         raise custom_error.CustomFlaskErr(status_code=500, message="验证码不能为空")
 
-    data_exists = mysql.get_list(user_sql.select_exists_user,(Account))
+    data_exists = mysql.get_list(user_sql.select_exists_user, (Account))
     if len(data_exists):
         raise custom_error.CustomFlaskErr(status_code=500, message="账号已经存在")
 
-    vocde_exists = mysql.get_object(verification_code_sql.select_verification_lastcode, (Account,1))
-    if vocde_exists== None :
+    vocde_exists = mysql.get_object(verification_code_sql.select_verification_lastcode, (Account, 1))
+    if vocde_exists == None:
         raise custom_error.CustomFlaskErr(status_code=500, message="验证码已失效")
-    if vocde_exists["VCode"]!=VCode:
+    if vocde_exists["VCode"] != VCode:
         raise custom_error.CustomFlaskErr(status_code=500, message="验证码不正确")
-    #设置验证码已读
-    mysql.operate_object(verification_code_sql.update_verificatioin_vcode,(vocde_exists["Id"]))
+    # 设置验证码已读
+    mysql.operate_object(verification_code_sql.update_verificatioin_vcode, (vocde_exists["Id"]))
 
     guid = str(uuid.uuid1())
-    sql_list = [user_sql.insert_user,user_sql.insert_userinfo]
-    args_list = [(guid,Account,Password,Account),(guid)]
+    sql_list = [user_sql.insert_user, user_sql.insert_userinfo]
+    args_list = [(guid, Account, Password, Account), (guid)]
 
-    data_register = mysql.operate__many(sql_list,args_list)
+    data_register = mysql.operate__many(sql_list, args_list)
     if data_register > 0:
+        # region 设置登陆信息，直接跳转到首页
+        guid = str(uuid.uuid1())
+        ip = request.remote_addr
+        count = mysql.operate_object(user_sql.update_user_token, (guid, ip, Account, Password))
+        user = mysql.get_object(user_sql.select_user_login_info, (guid))
+        session["user"] = user
+        data = {"token": guid, "datetime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), "user": user}
+
         ApiResponse.message = "注册成功"
         ApiResponse.status = 200
+        ApiResponse.data = data
         return api_response.response_return(ApiResponse)
-
     # date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     raise custom_error.CustomFlaskErr(status_code=500, message="注册失败")
-
-
-
