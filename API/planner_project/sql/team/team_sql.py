e3f376b48119964f675fd714ed7b2f62294185d3
xuxujiang0 <6895285a>
2017-09-06 22:12:25 +0800

    修改bug

--- API/planner_project/sql/team/team_sql.py
+++ API/planner_project/sql/team/team_sql.py
@@ -3,8 +3,9 @@ select_planner_team_member_list = "SELECT tm.UserId,ui.`Name`,ui.`HeadImage`,tm.
                                   "FROM `U_PlannerStatistics` ps " \
                                   "JOIN `T_TeamMember` tm ON ps.`UserId` = tm.`UserId` " \
                                   "JOIN `U_UserInfo` ui ON ui.`UserId`=tm.`UserId` " \
-                                  "WHERE ps.`UserId`='%s' AND tm.`IsDelete`= FALSE  AND ps.`IsDelete`= FALSE  AND ui.`IsDelete`= FALSE " \
-                                  "ORDER BY tm.`Sort` DESC,tm.`CreateTime` DESC "
+                                  "WHERE ps.`UserId`='%s' AND tm.`IsDelete`= FALSE  AND ps.`IsDelete`= FALSE  AND ui.`IsDelete`= FALSE  " \
+                                  "ORDER BY tm.`Sort` DESC,tm.`CreateTime` DESC " \
+                                  "LIMIT %s, %s "
 
 # 新增团队
 insert_team = "INSERT INTO `T_Team` (`Id`,`AdminUserId`,`Name`,`ServiceAreaId`,`ServiceDescription`,`CreateUserID`,`CreateTime`,`ModifUserID`,`ModifTime`) " \
