9cfa67f7eafb5b8eb7265ebd59355565217fa4fe
xuxujiang0 <6895285a>
2017-10-01 10:40:40 +0800

    修改bug

--- API/planner_project/sql/planner/planner_sql.py
+++ API/planner_project/sql/planner/planner_sql.py
@@ -9,7 +9,7 @@ select_top_planner = "SELECT ui.`UserId`,ui.`Name`,ui.`HeadImage`,ps.`Sort`,ps.`
 
 #查询规划师
 select_search_planner = "SELECT * FROM ( SELECT u.`Id`,ui.Sort,ui.`Name`,ui.`HeadImage`,ps.`NewEvaluate`,ps.`CustomerCount` " \
-                     ",ps.`PraiseCount`,ps.`BadReviewCount`,t.`Name` AS TeamName,Lables " \
+                     ",ps.`PraiseCount`,ps.`BadReviewCount`,t.`Name` AS TeamName,GROUP_CONCAT(ul.LableName) as Lables " \
                        "FROM `U_User` u " \
                        "LEFT JOIN `U_UserInfo` ui ON ui.`UserId`=u.`Id` " \
                        "LEFT JOIN `U_PlannerStatistics` ps ON ps.`UserId`=u.`Id` " \
@@ -20,7 +20,7 @@ select_search_planner = "SELECT * FROM ( SELECT u.`Id`,ui.Sort,ui.`Name`,ui.`Hea
                        "WHERE u.`UserType` IN (2,3) AND  u.`IsDelete`=FALSE AND ui.`IsDelete`=FALSE AND ps.`IsDelete`=FALSE " \
                        "AND ('%s' IS NULL OR '%s'='' OR ui.`Name` LIKE '%s' OR sa.`Name` LIKE '%s' OR st.`Name` LIKE '%s' OR ul.`LableName` LIKE '%s') " \
                         "GROUP BY u.`Id`) AS t "\
-                       "ORDER BY Sort Desc " \
+                       "ORDER BY Sort Desc,Id " \
                        "LIMIT %s , %s "
 #关注
 planner_follw="INSERT INTO `U_Follow` (`UserId`,`FollwUserId`,`FollwTime`) VALUES('%s','%s',NOW()) " \
